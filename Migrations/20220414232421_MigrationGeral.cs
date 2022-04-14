61aa341fb781d2737acad56711abc90e3abf4217
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-04-14 20:38:49 -0300

    Finalizada a aula 10

---
+++ Migrations/20220414232421_MigrationGeral.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rpg_Api.Migrations
+{
+    public partial class MigrationGeral : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habilidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dano = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habilidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Foto = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Latitude = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Longitude = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataAcesso = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PontosVida = table.Column<int>(type: "int", nullable: false),
+                    Forca = table.Column<int>(type: "int", nullable: false),
+                    Defesa = table.Column<int>(type: "int", nullable: false),
+                    Inteligencia = table.Column<int>(type: "int", nullable: false),
+                    Classe = table.Column<int>(type: "int", nullable: false),
+                    FotoPersonagem = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    UsuarioId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personagens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Personagens_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Armas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dano = table.Column<int>(type: "int", nullable: false),
+                    PersonagemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Armas_Personagens_PersonagemId",
+                        column: x => x.PersonagemId,
+                        principalTable: "Personagens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonagemHabilidades",
+                columns: table => new
+                {
+                    PersonagemId = table.Column<int>(type: "int", nullable: false),
+                    HabilidadeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonagemHabilidades", x => new { x.PersonagemId, x.HabilidadeId });
+                    table.ForeignKey(
+                        name: "FK_PersonagemHabilidades_Habilidades_HabilidadeId",
+                        column: x => x.HabilidadeId,
+                        principalTable: "Habilidades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonagemHabilidades_Personagens_PersonagemId",
+                        column: x => x.PersonagemId,
+                        principalTable: "Personagens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Habilidades",
+                columns: new[] { "Id", "Dano", "Nome" },
+                values: new object[,]
+                {
+                    { 1, 39, "Adormecer" },
+                    { 2, 41, "Congelar" },
+                    { 3, 37, "Hipnotizar" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Personagens",
+                columns: new[] { "Id", "Classe", "Defesa", "Forca", "FotoPersonagem", "Inteligencia", "Nome", "PontosVida", "UsuarioId" },
+                values: new object[,]
+                {
+                    { 1, 1, 23, 17, null, 33, "Frodo", 100, null },
+                    { 2, 1, 25, 15, null, 30, "Sam", 100, null },
+                    { 3, 3, 21, 18, null, 35, "Galadriel", 100, null },
+                    { 4, 2, 18, 18, null, 37, "Gandalf", 100, null },
+                    { 5, 1, 17, 20, null, 31, "Hobbit", 100, null },
+                    { 6, 3, 13, 21, null, 34, "Celeborn", 100, null },
+                    { 7, 2, 11, 25, null, 35, "Radagast", 100, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "DataAcesso", "Foto", "Latitude", "Longitude", "PasswordHash", "PasswordSalt", "Username" },
+                values: new object[] { 1, null, null, null, null, new byte[] { 104, 169, 228, 220, 147, 42, 195, 61, 64, 226, 37, 48, 103, 75, 62, 63, 128, 206, 112, 27, 219, 209, 133, 179, 173, 116, 60, 6, 233, 115, 165, 232, 73, 232, 130, 90, 99, 154, 247, 65, 222, 3, 252, 104, 234, 14, 129, 125, 21, 186, 176, 232, 18, 66, 184, 109, 109, 244, 125, 138, 205, 26, 116, 56 }, new byte[] { 134, 210, 13, 95, 87, 52, 29, 229, 134, 30, 133, 246, 85, 82, 4, 138, 115, 60, 163, 48, 144, 11, 219, 29, 239, 69, 180, 176, 75, 164, 3, 173, 242, 190, 3, 85, 116, 36, 18, 101, 54, 232, 148, 106, 51, 183, 184, 209, 205, 242, 106, 217, 246, 230, 141, 63, 159, 59, 122, 178, 1, 223, 194, 45, 143, 88, 29, 208, 237, 97, 118, 89, 231, 207, 89, 203, 180, 126, 128, 120, 239, 125, 93, 116, 121, 107, 159, 176, 31, 91, 127, 223, 9, 57, 205, 240, 237, 232, 75, 71, 113, 67, 89, 100, 71, 65, 15, 247, 130, 220, 83, 146, 108, 190, 90, 231, 143, 221, 52, 160, 119, 91, 110, 166, 105, 182, 103, 87 }, "UsuarioAdmin" });
+
+            migrationBuilder.InsertData(
+                table: "Armas",
+                columns: new[] { "Id", "Dano", "Nome", "PersonagemId" },
+                values: new object[,]
+                {
+                    { 1, 35, "Arco e Flecha", 1 },
+                    { 2, 33, "Espada", 2 },
+                    { 3, 31, "Machado", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PersonagemHabilidades",
+                columns: new[] { "HabilidadeId", "PersonagemId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 3, 3 },
+                    { 3, 4 },
+                    { 1, 5 },
+                    { 2, 6 },
+                    { 3, 7 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Armas_PersonagemId",
+                table: "Armas",
+                column: "PersonagemId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonagemHabilidades_HabilidadeId",
+                table: "PersonagemHabilidades",
+                column: "HabilidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personagens_UsuarioId",
+                table: "Personagens",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Armas");
+
+            migrationBuilder.DropTable(
+                name: "PersonagemHabilidades");
+
+            migrationBuilder.DropTable(
+                name: "Habilidades");
+
+            migrationBuilder.DropTable(
+                name: "Personagens");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}

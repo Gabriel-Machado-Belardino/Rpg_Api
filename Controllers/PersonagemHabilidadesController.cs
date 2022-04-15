e403b5acc4ac3df1483dcceda734d19d95836d9d
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-04-14 22:43:55 -0300

    Progress too step 14 but with some errors

---
+++ Controllers/PersonagemHabilidadesController.cs
@@ -0,0 +1,66 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Rpg_Api.Data;
+
+namespace Rpg_Api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonagemHabilidadesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PersonagensExemploController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddPersonagemHabilidadeAsync(PersonagemHabilidade novoPersonagemHabilidade)
+        {
+            try
+            {
+                Personagem personagem = await _context.Personagens
+                .Include(p => p.Arma)
+                .Include(p => p.PersonagemHabilidades).ThenInclude(ps => ps.Habilidade)
+                .FirstOrDefaultAsync(p => p.Id == novoPersonagemHabilidade.PersonagemId);
+
+                if(personagem == null)
+                    throw new System.Exception("Personagem não encontrado para o Id informado.");
+                
+                Habilidade habilidade = await _context.Habilidades.FirstOrDefaultAsync(h => h.Id == novoPersonagemHabilidade.HabilidadeId);
+
+                if(habilidade == null)
+                    throw new System.Exception("Habilidade não encontrada.");
+
+                PersonagemHabilidade ph = new PersonagemHabilidade();
+                ph.Personagem = personagem;
+                ph.Habilidade = habilidade;
+                await _context.PersonagemHabilidades.AddAsync(ph);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+    }
+}
+
+/*
+    try
+    {
+        
+        return Ok();
+    }
+    catch(System.Exception ex)
+    {
+        return BadRequest(ex.Message);
+    }
+*/
